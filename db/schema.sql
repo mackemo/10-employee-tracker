626d4ac8a0c9ca244bb298b783e7191b3b5ba60a
mackemo <mackenzielmoore14@gmail.com>
2024-05-30 11:52:07 -0400

    created a database called company_db, created three tables for departments, roles, and employees

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+-- delete exsiting database and create a new one
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+-- use this database
+\c company_db;
+
+-- created table for departments
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL,
+);
+
+-- create table for roles
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOR KEY department_id
+    REFERENCES departments(id)
+    ON DELETE CASCADE
+);
+
+-- create table for employees
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOR KEY role_id REFERENCES roles(id) ON DELETE CASCADE,
+    FOR KEY manager_id REFERENCES employees(id) ON DELETE SET NULL
+);
