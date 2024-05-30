92c5ef8368067c945b13c083192ab0af59166449
mackemo <mackenzielmoore14@gmail.com>
2024-05-30 12:30:38 -0400

    fixed typos, then created query.sql file to join tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
--- delete exsiting database and create a new one
+-- delete existing database and create a new one
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
@@ -8,7 +8,7 @@ CREATE DATABASE company_db;
 -- created table for departments
 CREATE TABLE departments (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 -- create table for roles
@@ -17,7 +17,7 @@ CREATE TABLE roles (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOR KEY department_id
+    FOREIGN KEY department_id
     REFERENCES departments(id)
     ON DELETE CASCADE
 );
@@ -29,6 +29,6 @@ CREATE TABLE employees (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOR KEY role_id REFERENCES roles(id) ON DELETE CASCADE,
-    FOR KEY manager_id REFERENCES employees(id) ON DELETE SET NULL
+    FOREIGN KEY role_id REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY manager_id REFERENCES employees(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -10,11 +10,11 @@ VALUES
 -- inserting 5 roles and data
 INSERT INTO roles (title, salary, department_id)
 VALUES
-    ('Accountant', '65000', 1),
-    ('Digital Marketing Specialist', '50000', 2),
-    ('Logistics Coordinator', '45000', 3),
-    ('HR Generalist', '60000', 4),
-    ('Systems Analyst', '70000', 5);
+    ('Accountant', 65000, 1),
+    ('Digital Marketing Specialist', 50000, 2),
+    ('Logistics Coordinator', 45000, 3),
+    ('HR Generalist', 60000, 4),
+    ('Systems Analyst', 70000, 5);
 
 -- inserting 5 employees and data
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
