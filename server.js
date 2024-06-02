f3357af8d99256329b70b649f0131363cd1e856f
mackemo <mackenzielmoore14@gmail.com>
2024-06-02 13:02:03 -0400

    added port, instance of express and middleware

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express');
+// creating connection pool to connect application to server
 const { POOL } = require('pg');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+
+
